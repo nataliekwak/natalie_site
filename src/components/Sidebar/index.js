ccb0ca8930e2c652e4ff0a9bad5b69837549d97b
Natalie <natalie.ann.kwak@gmail.com>
2024-09-24 16:02:12 -0400

    updated navbar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,43 +1,41 @@
 import { NavLink } from 'react-router-dom';
 import './index.scss';
 
-// import LogoS from '../../assets/images/logo-s.png';
-// import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faFileLines, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { Tooltip } from '@mui/material';
 
 const Sidebar = () => {
     return (
         <div className='nav-bar'>
-            {/* <Link className='logo' to='/'>
-                <img src={LogoS} alt='logo' />
-                <img className='sub-logo' src={LogoSubtitle} alt='natalie' />
-            </Link> */}
-
             <nav>
                 <NavLink exact='true' activeclassname='active' to='/'>
-                    <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
+                    <FontAwesomeIcon icon={faHome} color='#484847' />
                 </NavLink>
                 <NavLink exact='true' activeclassname='active' className='about-link' to='/about'>
-                    <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
+                    <FontAwesomeIcon icon={faUser} color='#484847' />
                 </NavLink>
                 <NavLink exact='true' activeclassname='active' className='contact-link' to='/contact'>
-                    <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
+                    <FontAwesomeIcon icon={faEnvelope} color='#484847' />
                 </NavLink>
             </nav>
             <ul>
                 <li>
-                    <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/natalie-kwak/'>
-                        <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+                    <a target='_blank' title='Resume' rel='noreferrer' href='https://github.com/nataliekwak'>
+                        <FontAwesomeIcon icon={faFileLines} color='#484847' />
                     </a>
                 </li>
                 <li>
-                    <a target='_blank' rel='noreferrer' href='https://github.com/nataliekwak'>
-                        <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+                    <a target='_blank' title='LinkedIn' rel='noreferrer' href='https://www.linkedin.com/in/natalie-kwak/'>
+                        <FontAwesomeIcon icon={faLinkedin} color='#484847' />
                     </a>
                 </li>
-                {/* TO DO: make a little paper icon and link resume */}
+                <li>
+                    <a target='_blank' title='Github' rel='noreferrer' href='https://github.com/nataliekwak'>
+                        <FontAwesomeIcon icon={faGithub} color='#484847' />
+                    </a>
+                </li>
             </ul>
         </div>
     )
